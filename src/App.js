781ae85ac20157557f1b978dc9bbb0278b0af631
AlForuq <muhammad.alforuq@gmail.com>
2023-04-26 16:14:59 +0500

    multi-action in single hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({ count: 0, name: "" });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Count: {data.count}</h1>
+        <h1>Name:{data.name}</h1>
+        <button onClick={() => setData({ ...data, count: data.count + 1 })}>
+          +
+        </button>
+        <button onClick={() => setData({ ...data, count: data.count - 1 })}>
+          -
+        </button>
+
+        <input
+          onChange={({ target: { value } }) =>
+            setData({ ...data, name: value })
+          }
+        />
+      </div>
     </div>
   );
 }
