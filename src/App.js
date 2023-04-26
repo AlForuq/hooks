0c47908f33b0784cb22879a643cdad959e95b546
AlForuq <muhammad.alforuq@gmail.com>
2023-04-26 15:52:22 +0500

    use-effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Hook from "./Hooks";
 
 function App() {
+  const [count, setCount] = useState(1);
+  const [name, setName] = useState("web");
+
+  // case 1
+  useEffect(() => {
+    console.log("case 1");
+  });
+
+  // case 2
+  useEffect(() => {
+    console.log("case 2");
+  }, []);
+
+  // case 3
+  useEffect(() => {
+    console.log("case 3");
+  }, [count]);
+
+  // case 4
+  useEffect(() => {
+    console.log("case 4");
+  }, [count, name]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Count: {count}</h1>
+        <h1>Name:{name}</h1>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+
+        <input onChange={({ target: { value } }) => setName(value)} />
+      </div>
+      <Hook handleName={name} />
     </div>
   );
 }
